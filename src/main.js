774a302c7f5f51d5e785ce594c4b57a04a039365
hamdiranu <hamdira@sci.ui.ac.id>
2021-08-06 17:50:30 +0700

    add small project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
